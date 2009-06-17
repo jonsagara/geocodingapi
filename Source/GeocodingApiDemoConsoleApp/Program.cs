42f7ec02671d308b454ba2e9b5ec0f7d371fca54
jonsagara <jonsagara@4229e56a-5b4f-11de-b36e-3fc19fc75de0>
2009-06-17 15:52:12 +0000

    Added a demo console app.

---
+++ Source/GeocodingApiDemoConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GeocodingApi;
+
+namespace GeocodingApiDemoConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+#if DEBUG
+			Console.WriteLine("Press ENTER to quit the application");
+			Console.ReadLine();
+#endif
+		}
+	}
+}
