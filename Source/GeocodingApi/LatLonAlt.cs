5f5fc9e3f9b14e51a09cca1922cd383f73e3ba8a
jonsagara <jonsagara@4229e56a-5b4f-11de-b36e-3fc19fc75de0>
2009-06-27 20:44:36 +0000

    Added new client-facing classes

---
+++ Source/GeocodingApi/LatLonAlt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GeocodingApi
+{
+	[Serializable]
+	public class LatLonAlt
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+		public double Altitude { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format(
+				"{0}, {1}, {2}",
+				Latitude,
+				Longitude,
+				Altitude
+				);
+		}
+	}
+}
