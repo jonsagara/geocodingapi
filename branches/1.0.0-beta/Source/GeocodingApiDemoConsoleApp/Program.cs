2f968bc8549d6a9ac5a3bc6b272553768a86dfa3
jonsagara <jonsagara@4229e56a-5b4f-11de-b36e-3fc19fc75de0>
2009-07-05 20:57:28 +0000

    Now works on a List of addresses instead of just a single address.

--- branches/1.0.0-beta/Source/GeocodingApiDemoConsoleApp/Program.cs
+++ branches/1.0.0-beta/Source/GeocodingApiDemoConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Text;
 
 using GeocodingApi;
@@ -9,33 +10,53 @@ namespace GeocodingApiDemoConsoleApp
 {
 	class Program
 	{
+		#region Class Data
+
+		private static readonly List<string> Addresses = new List<string>
+		{
+			"1600 Amphitheatre Parkway, Mountain View, CA 94043",		// Google
+			"1 Microsoft Way, Redmond, WA 98052",						// Microsoft
+			"1601 S. California Ave., Palo Alto, CA 95304",				// Facebook
+			"701 First Ave, Sunnyvale, CA 94089"						// Yahoo
+		};
+
+		/// <summary>
+		/// The API is rate limited.  1 request every 0.5 seconds seems to keep Google happy.
+		/// </summary>
+		private static readonly int DelayInMs = 500;
+
+		#endregion
+
+
 		static void Main(string[] args)
 		{
-			string address = "1600 Amphitheatre Parkway, Mountain View, CA 94043";
-			List<GeographicCoordinate> coords = Geocoding.Geocode(address);
+			Addresses.ForEach(
+				address =>
+				{
+					Geocoding.Geocode(address)
+						.ForEach(coordinate => DisplayCoordinate(coordinate, address));
 
-			Console.WriteLine("Coordinates for address \"{0}\":", address);
-			if (coords.Count == 0)
-			{
-				Console.WriteLine("\tNone returned for this address");
-			}
-			else
-			{
-				coords.ForEach(
-					coord =>
-						Console.WriteLine(
-							"\t* Latitude: {0}, Longitude: {1}, Altitude: {2}",
-							coord.Latitude,
-							coord.Longitude,
-							coord.Altitude
-							)
-					);
-			}
+					Thread.Sleep(DelayInMs);
+				}
+			);
 
+			#region DEBUG only
 #if DEBUG
 			Console.WriteLine("Press ENTER to quit the application");
 			Console.ReadLine();
 #endif
+			#endregion
+		}
+
+
+		private static void DisplayCoordinate(GeographicCoordinate coordinate, string address)
+		{
+			Console.WriteLine("Geographic coordinate for '{0}':", address);
+			Console.WriteLine(
+				"\t* Latitude: {0}, Longitude: {1}",
+				coordinate.Latitude,
+				coordinate.Longitude
+				);
 		}
 	}
 }
