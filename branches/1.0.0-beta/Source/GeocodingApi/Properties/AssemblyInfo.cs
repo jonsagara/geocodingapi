b607fb24339ff1fc93a11f33fcc1e85a13529e5f
jonsagara <jonsagara@4229e56a-5b4f-11de-b36e-3fc19fc75de0>
2009-07-05 21:16:41 +0000

    Marked the assembly as CLSCompliant.

--- branches/1.0.0-beta/Source/GeocodingApi/Properties/AssemblyInfo.cs
+++ branches/1.0.0-beta/Source/GeocodingApi/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GeocodingApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# Wrapper of the Google Maps geocoding service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GeocodingApi")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCopyright("Copyright © 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
